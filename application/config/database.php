ebf550ed03b595bb8e0ce5103a39915a8da4f860
pilopa <pilopaokta@gmail.com>
2017-07-06 08:51:46 +0700

    readme

--- application/config/database.php
+++ application/config/database.php
@@ -69,6 +69,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
+test
 */
 $active_group = 'default';
 $query_builder = TRUE;
